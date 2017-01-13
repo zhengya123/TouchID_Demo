9b9e5bf7a683534dcb46eda0ab073befe4428511
zhengya123 <919458797@qq.com>
2017-01-13 11:45:49 +0800

    firstCommit

--- TouchID_Demo/ViewController.m
+++ TouchID_Demo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <LocalAuthentication/LocalAuthentication.h>
 @interface ViewController ()
 
 @end
@@ -16,14 +16,107 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    UILabel * label = [[UILabel alloc]initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 100)];
+    label.text = @"点击屏幕开始\n 指纹输入不对会出现密码";
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
 }
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self evaluateAuthenticate];
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
+#pragma mark - 验证指纹退出APP
+/**
+ * 指纹验证
+ */
+- (void)evaluateAuthenticate
+{
+    //创建LAContext
+    LAContext* context = [[LAContext alloc] init];
+    NSError* error = nil;
+    NSString* result = @"请验证已有指纹";
+    //首先使用canEvaluatePolicy 判断设备支持状态
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        //支持指纹验证
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:result reply:^(BOOL success, NSError *error) {
+            if (success) {
+                
+                [[UIApplication sharedApplication]performSelector:@selector(suspend)];
+            }
+            else
+            {
+                NSLog(@"%@",error.localizedDescription);
+                switch (error.code) {
+                    case LAErrorUserFallback:
+                    {
+                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                            
+                            NSLog(@"用户选择输入密码，切换主线程处理");
+                             [self  authentication];
+                        }];
+                        break;
+                    }
+                    default:
+                    {
+                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                            
+                            
+                            NSLog(@"其他情况，切换主线程处理");
+                        }];
+                        break;
+                    }
+                }
+            }
+        }];
+    }
+    else
+    {
+        //不支持指纹识别，LOG出错误详情
+        NSLog(@"不支持指纹识别");
+        
+        switch (error.code) {
+            case LAErrorTouchIDNotEnrolled:
+            {
+                NSLog(@"TouchID is not enrolled");
+                break;
+            }
+            case LAErrorPasscodeNotSet:
+            {
+                NSLog(@"A passcode has not been set");
+                break;
+            }
+            default:
+            {
+                NSLog(@"TouchID not available");
+                break;
+            }
+        }
+        
+        NSLog(@"%@",error.localizedDescription);
+    }
+}
+/**
+ *  密码验证
+ */
+- (void)authentication
+{
+    //创建LAContext
+    LAContext* context = [[LAContext alloc] init];
+    NSError* error = nil;
+    //首先使用canEvaluatePolicy 判断设备支持状态
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthentication localizedReason:@"验证信息" reply:^(BOOL success, NSError *error) {
+            if (success) {
+                
+                NSLog(@"验证成功，主线程处理UI");
+            }
+            else
+            {
+                NSLog(@"%@",error.localizedDescription);
+            }
+        }];
+    }
+}
 
 @end
